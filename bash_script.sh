8aad493952f81daecadbfab469d65d876a4ad98e
claudiodsjr <claudiodsjr@hotmail.com>
2020-09-29 16:30:16 +0000

    Prova 1.2

--- bash_script.sh
+++ bash_script.sh
@@ -1,5 +1,11 @@
+#Comando que mostra a pasta/diretório onde estou
+
 pwd
+
+#Comando que gera uma lista com detalhes do conteúdo da minha pasta home e redireciona para o arquivo list_HOME.txt
 
 ls -alF ~/ > list_HOME.txt 
 
-cat < lista_HOME.txt
+#Comando que imprime a data atual na tela
+
+date -u
