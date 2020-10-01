9982dda1ae18972647950d356c9d57dc812ff7e9
claudiodsjr <claudiodsjr@hotmail.com>
2020-10-01 20:51:14 +0000

    Prova 1.4

--- script_hipotenusa.py
+++ script_hipotenusa.py
@@ -2,22 +2,18 @@
 import sys
 import math
 
-#Associando os dados que serão colocados na linha de comando a variáveis e transformando-os em inteiros.
+#Começo avaliando se as variáveis são numéricas ou não. Se forem numéricas sigo para a transformação destas em inteiros e então a avaliação se são positivas e menores que 1000, caso atinjam estas condições, faço o calculo da hipotenusa, caso não atinjam, digo que as variáveis não atendem a necessidade de serem positivas e/ou menores que 1000. Caso não sejam números, aviso que as variáveis não são numéricas.
 
-number1 = int(sys.argv[1]) 
-number2 = int(sys.argv[2])
-
-#Utilizando o comando isinstance para confirmar que as variáveis se tratam de números inteiros após a transformação acima.
-
-print(isinstance(number1, int))
-print(isinstance(number2, int))
-
-#Utilizando o comando if para garantir que o cálculo só será realizado se as variáveis forem positivas (<= 0) e menores que 1000 e printando a resposta no caso do cálculo com a hipotenusa abreviada para duas casas depois da vírgula. Caso o valor não atinja as condições, diz-se que as variáveis não atendem os requisitos.
-
-if 0 <= number1 < 1000 and 0 <= number2 < 1000:
-	hip = math.sqrt((number1**2) + (number2**2))
-	print('O quadrado da hipotenusa do triângulo retângulo com os lados a = {0} e b = {1}, é {2}.'.format(number1, number2, round( hip, 2)))
+if sys.argv[1].isdigit() == True and sys.argv[2].isdigit() == True:
+	number1 = int(sys.argv[1]) 
+	number2 = int(sys.argv[2])
+	print('As variáveis são numéricas.')
+	if 0 <= number1 < 1000 and 0 <= number2 < 1000:
+		hip = math.sqrt((number1**2) + (number2**2))
+		print('O quadrado da hipotenusa do triângulo retângulo com os lados a = {0} e b = {1}, é {2}.'.format(number1, number2, round(hip, 2)))
+	else:
+		print('As variáveis não atendem aos requisitos!')
 else:
-	print('As variáveis não atendem os requisitos!')
+	print('Variáveis não numéricas ou negativas.')
 
 
