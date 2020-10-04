980140740632d1144d955b02af666989d15cc810
claudiodsjr <claudiodsjr@hotmail.com>
2020-10-04 17:24:44 +0000

    Prova 1.9

--- script_hipotenusa.py
+++ script_hipotenusa.py
@@ -2,7 +2,7 @@
 import sys
 import math
 
-#Começo avaliando se as variáveis são numéricas ou não. Se forem numéricas sigo para a transformação destas em inteiros e então a avaliação se são positivas e menores que 1000, caso atinjam estas condições, faço o calculo da hipotenusa, caso não atinjam, digo que as variáveis não atendem a necessidade de serem positivas e/ou menores que 1000. Caso não sejam números, aviso que as variáveis não são numéricas.
+#Começo avaliando se as variáveis são numéricas ou não. Se forem numéricas sigo para a transformação destas em inteiros e então a avaliação se são positivas e menores que 1000, caso atinjam estas condições, faço o calculo do quadrado da hipotenusa, caso não atinjam, digo que as variáveis não atendem a necessidade de serem positivas e/ou menores que 1000. Caso não sejam números, aviso que as variáveis não são numéricas.
 
 if sys.argv[1].isdigit() == True and sys.argv[2].isdigit() == True:
 	number1 = int(sys.argv[1]) 
@@ -14,7 +14,7 @@ if sys.argv[1].isdigit() == True and sys.argv[2].isdigit() == True:
 		hip = (number1**2) + (number2**2)
 		print('O quadrado da hipotenusa do triângulo retângulo com os lados a = {0} e b = {1}, é {2}.'.format(number1, number2, round(hip, 2)))
 	else:
-		print('As variáveis não atendem aos requisitos!')
+		print('As variáveis não atendem aos requisitos (pelo menos um valor maior que 1000)!')
 else:
 	print('Variáveis não numéricas, não inteiras ou negativas.')
 
